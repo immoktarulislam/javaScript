c2ef984de953196d3940296aa093b06baeb61b84
immoktarulislam <moktarul888@gmail.com>
2026-07-21 00:02:51 +0530

    loopsJS

---
+++ loops.js
@@ -0,0 +1,17 @@
+// // let age = 10;
+// // if (age >= 18) {
+// //     console.log("You are an adult.");
+// // } else {
+// //     console.log("You are a minor.");
+// // }
+
+// let age = 2;
+// if (age >= 18) {
+//     console.log("You are an adult.");
+// } else if (age >= 13) {
+//     console.log("You are a teenager.");
+// } else if (age >= 10) {
+//     console.log("You can be a pre-teen.");
+// } else {
+//     console.log("You are a young child.");
+// }
