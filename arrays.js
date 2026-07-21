12131692e5b338fb72e4dd10fbd04e102fd65cdc
immoktarulislam <moktarul888@gmail.com>
2026-07-21 22:36:38 +0530

    arrays.js

---
+++ arrays.js
@@ -0,0 +1,15 @@
+// let salary = [45000, 35000, 50000, 60000, 70000];
+// // let totalNum = salary.length;
+// // console.log(totalNum);
+// // salary.push(80000);
+// // salary.pop();
+// // salary.shift();
+// // console.log(salary);
+// salary.unshift(30000);
+// console.log(salary);
+
+let abid = [2, 6, 9]
+let sabid = [12, 13, 14]
+let concatArray = abid.concat(sabid);
+let arrSlice = concatArray.slice(0, 3)
+console.log(arrSlice);
---
+++ condition.js
@@ -0,0 +1,41 @@
+// NOrmal condition
+
+// const salary = 45000
+// const isBcs = false
+// const hasCar = false
+// const height = 6.2
+// if ((salary > 35000 && height > 6.0) || (isBcs == true && hasCar == true)) { console.log("You are eligible for the loan."); }
+// // if (salary > 20000 && isBcs && hasCar && height > 5.5) {
+// //     console.log("You are eligible for the loan.");
+// // }
+// else {
+//     console.log("You are not eligible for the loan.");
+// }
+
+
+// const isWebDeveloper = true;
+// const isEarnPerMonthUsd = 3500
+// const isSoftSkill = true
+// const isEnglishFluent = true
+// const isReligiousPerson = true
+
+// if (isReligiousPerson == true && isEarnPerMonthUsd > 3400) { console.log("You are eligible for the job."); }
+// else { console.log("You are not eligible for the job."); }
+
+// DISCOUNT PRACTICE
+// const price = 5000;
+// if (price > 4999) {
+//     const discount = 0.1;
+//     const discountedPrice = price - (price * discount);
+//     console.log(`The discounted price is: ${discountedPrice}`);
+// }
+
+const price = 5000;
+if (price > 4999) {
+    const discount = price * 5 / 100;
+    const discountedPrice = price - discount;
+    const payBill = price - discount;
+    J    // console.log(`You have to pay: ${payBill}`);
+    console.log(`You have to pay`, payBill);
+
+}
