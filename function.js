80a378dbc1096b604bb3f24c0f404244705239b5
immoktarulislam <moktarul888@gmail.com>
2026-07-29 23:35:45 +0530

    assign 2 task 1

--- function.js
+++ function.js
@@ -1,7 +1,49 @@
-function bazar() {
-    console.log(`aj sunday anek kaz bazr korte hobbe `)
+// function bazar() {
+//     console.log(`aj sunday anek kaz bazr korte hobbe `)
+// }
+// bazar()
+// bazar()
+// bazar()
+// bazar(`egg sugar musturd oil`)
+
+// const teamAGoals = 5;
+// const teamBGoals = `6`;
+// if (teamAGoals > teamBGoals) {
+//     console.log(`Team A won`)
+// }
+// else if (teamAGoals < teamBGoals) {
+//     console.log(`Team B won`)
+// }
+// else if (typeof teamAGoals !== `number` && typeof teamBGoals !== `number`) {
+//     console.log(`invalid`)
+// }
+// else {
+//     console.log(`Drow`)
+// }
+
+// function square(num) {
+//     if (typeof num !== `number`) {
+//         return `error: please provide a number`
+//     }
+//     return num * num
+// }
+// console.log(square(9))
+// console.log(square(`9`))
+
+function matchWinner(teamAGoals, teamBGoals) {
+    if (teamAGoals > teamBGoals) {
+        return `Team A won`
+    }
+    else if (teamAGoals < teamBGoals) {
+        return `Team B won`
+    } else if (typeof teamAGoals !== `number`) {
+        return `invalid`
+    }
+    else if (typeof teamBGoals !== `number`) {
+        return `invalid`
+    }
+    else {
+        return `Drow`
+    }
 }
-bazar()
-bazar()
-bazar()
-bazar(`egg sugar musturd oil`)
+console.log(matchWinner(5, `5`))
