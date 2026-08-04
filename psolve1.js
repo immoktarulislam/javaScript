bb6ff5311adf74f534e9c8da7dec9e40a5be6fef
immoktarulislam <moktarul888@gmail.com>
2026-08-04 23:38:28 +0530

    problem solving basic task

--- psolve1.js
+++ psolve1.js
@@ -6,7 +6,51 @@
 // console.log(sum(7, 7))
 
 //even or odd number
-function isEven(num) {
-    return num % 2 === 0;
+// function isEven(num) {
+//     return num % 2 === 0;
+// }
+// console.log(isEven(25))
+// console.log(isEven(-21))
+
+//print largest number
+// function largest(a, b, c) {
+//     if (a >= b && a >= c) {
+//         // console.log(`this is big`)
+//         return a
+//     } else if (b >= a && b >= c) {
+//         // console.log(`this is big`);
+//         return b
+//     } else {
+//         // console.log(`this is big`);
+//         return c
+//     }
+// }
+// console.log(`this is big ${largest(-12, -25, -13)} ,between the numbers`)
+
+// reverse str
+// function reverse(str) {
+//     return str.split(``).reverse().join(``);
+// }
+// console.log(reverse(`hello`))
+
+//loop method
+// function reverse(str) {
+//     let result = ``;
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         result += str[i];
+//     } return result
+// }
+// console.log(reverse(`hello`))
+
+//count vowel
+function countVowels(str) {
+    let count = 0;
+    let vowels = `aeiouAEIOU`;
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count
 }
-console.log(isEven(25))
+console.log(countVowels(`beautiful`))
